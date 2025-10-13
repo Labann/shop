3d275e3ef09f80703bb7b9aacc67c654026a84bc
Labann <labanmusen32@gmail.com>
2025-10-13 09:45:27 +0300

    same site strict

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -72,7 +72,7 @@ export const redirectToClientHome: express.RequestHandler = async (req, res) => 
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "none",
+            sameSite: "strict",
             maxAge: 15 * 24 * 60 * 60 * 1000 
         });
         
