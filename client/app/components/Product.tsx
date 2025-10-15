dd393abb01ad926ec92238367beaf88b5ff37d21
Labann <labanmusen32@gmail.com>
2025-10-15 14:22:26 +0300

    fill-red

--- client/app/components/Product.tsx
+++ client/app/components/Product.tsx
@@ -18,9 +18,10 @@ const Product = ({product}:{
     <div className="flex flex-col space-y-2 max-w-15em">
         <div className="relative bg-gray-100 h-[12em] w-full md:h-[15em] flex justify-center items-center">
             <div className="rounded-full bg-white p-1 absolute top-2 right-2">
-                {wish && wish.some(item => item.id === product.id) && <GrFavorite onClick={() => dispatch(unWish(product))} className='cursor-pointer bg-red-600 overflow-hidden'/>}
-            
-                {wish && wish.some(item => item.id !== product.id) && <GrFavorite onClick={() => dispatch(makeWish(product))} className='cursor-pointer'/>}
+                {wish && wish.some(item => item.id === product.id)?  (
+                    <GrFavorite onClick={() => dispatch(unWish(product))} className='cursor-pointer fill-red-600 overflow-hidden'/>)
+                : <GrFavorite onClick={() => dispatch(makeWish(product))} className='cursor-pointer'/>}
+                
                 {!wish && <GrFavorite onClick={() => dispatch(makeWish(product))} className='cursor-pointer'/>}
                 {wish && wish.length === 0 && <GrFavorite onClick={() => dispatch(makeWish(product))} className='cursor-pointer'/>}
             </div>
