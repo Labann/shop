fc065c49999f356a14f3017c7826d535f94be190
Labann <labanmusen32@gmail.com>
2025-10-15 08:47:46 +0300

    w-fit

--- client/app/products/page.tsx
+++ client/app/products/page.tsx
@@ -54,7 +54,7 @@ const Products = () => {
                     placeholder='Search products'
                     />
             </div>
-            <div className="mx-auto py-6 flex flex-col items-center sm:grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+            <div className="mx-auto py-6 w-fit  grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
                 {
                     products.length === 0 && !isLoading && (
                         <p className="font-bold text-center text-primary">No products found</p>
