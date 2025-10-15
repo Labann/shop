d43d5502776f85e4816978daf15966510e4b3f1a
Labann <labanmusen32@gmail.com>
2025-10-15 08:57:51 +0300

    made w- of product fixed

--- client/app/components/Product.tsx
+++ client/app/components/Product.tsx
@@ -11,7 +11,7 @@ const Product = ({product}:{
 }) => {
     if(!product) return;
   return (
-    <div className="flex flex-col space-y-2 w-15em">
+    <div className="flex flex-col space-y-2 max-w-15em">
         <div className="relative bg-gray-100 h-[12em] w-full md:h-[15em] flex justify-center items-center">
             <div className="rounded-full bg-white p-1 absolute top-2 right-2">
                 <GrFavorite className='cursor-pointer'/>
