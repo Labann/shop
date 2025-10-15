f170573d14eabe6d21df54c09d9407d744a45bce
Labann <labanmusen32@gmail.com>
2025-10-15 16:39:54 +0300

    isnot loading

--- client/app/categories/[name]/page.tsx
+++ client/app/categories/[name]/page.tsx
@@ -21,7 +21,7 @@ const Categories = () => {
   return (
     <div className='max-w-7xl mx-auto min-h[75vh]'>
         <h1><Link href={"/"}>home</Link> / <span className='font-bold text-primary text-2xl'>{name}</span></h1>
-        {products.length === 0 && <span className="font-bold text-primary">no products of type {name} found</span>}
+        {!isLoading && products.length === 0 && <span className="font-bold text-primary">no products of type {name} found</span>}
         <div className="grid grid-cols-1 w-fit sm:w-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-6 gap-7 mx-auto">
           {
               isLoading  && (
