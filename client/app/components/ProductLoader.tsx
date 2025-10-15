734a79320ba8af740d2e43194952989071737405
Labann <labanmusen32@gmail.com>
2025-10-15 08:13:57 +0300

    bg-gray-100

--- client/app/components/ProductLoader.tsx
+++ client/app/components/ProductLoader.tsx
@@ -3,14 +3,15 @@
 const ProductLoader = () => {
     return(
         <div className="w-[15em] h-[18em] flex flex-col space-y-3">
-            <div className="flex-1 bg-slate-300 animate-pulse w-full">
+            <div className="flex-1 bg-gray-100 animate-pulse w-full">
             </div>
-            <div className="w-[5em] h-[2em] rounded-md bg-slate-300 animate-pulse">
+            <div className="w-[5em] h-[2em] rounded-md bg-gray-100 animate-pulse">
 
             </div>
-            <div className="w-[7em] rounded-md h-[2em] bg-slate-300 animate-pulse">
+            <div className="w-[7em] rounded-md h-[2em] bg-gray-100 animate-pulse">
                 
             </div>
+            <div className="w-full bg-gray-100 animate-pulse rounded p-3"></div>
         </div>
     )
 }
