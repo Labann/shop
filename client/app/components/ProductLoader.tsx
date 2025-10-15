ec10d3017872f5df012ed32a2c4336038fb6d75a
Labann <labanmusen32@gmail.com>
2025-10-15 08:11:51 +0300

    loader h

--- client/app/components/ProductLoader.tsx
+++ client/app/components/ProductLoader.tsx
@@ -2,8 +2,8 @@
 
 const ProductLoader = () => {
     return(
-        <div className="w-[15em] h-[18em]">
-            <div className="h-1/3 bg-slate-300 animate-pulse w-full">
+        <div className="w-[15em] h-[18em] flex flex-col space-y-3">
+            <div className="flex-1 bg-slate-300 animate-pulse w-full">
             </div>
             <div className="w-[5em] h-[2em] rounded-md bg-slate-300 animate-pulse">
 
