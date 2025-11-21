fe08ca46f985720a036ae7ca64ab300ee0b31281
Labann <labanmusen32@gmail.com>
2025-11-21 09:59:23 +0300

    font added

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -33,7 +33,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
       >
         <Providers>
             {
